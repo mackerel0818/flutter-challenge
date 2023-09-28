eb35967fe9aec884a9dd2abc90bd91b38e624e8e
makerel0818 <minji2700@naver.com>
2023-09-28 21:52:47 +0900

    update: api

--- flutter-challenge/lib/main.dart
+++ flutter-challenge/lib/main.dart
@@ -1,61 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:toonfix/widgets/button.dart';
-import 'package:toonfix/widgets/currency_card.dart';
+import 'package:toonfix/screens/home_screen.dart';
+import 'package:toonfix/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(App());
 }
 
-class App extends StatefulWidget {
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          headline5: TextStyle(
-            color: Colors.red,
-          ),
-        ),
-      ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.headline5?.color,
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
